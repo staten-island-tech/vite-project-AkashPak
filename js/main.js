e9ff18bb6dba945f827242b9bf4113d030842aaf
AkashPak <akashp8@nycstudents.net>
2023-11-21 08:44:25 -0500

    dawds

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { DomSelectors } from './Dom.js'
-import { Menu } from `./menu`
+import { Menu } from `./menu.js`
 
 
 
 document.querySelector(".whole").addEventListener("click",`
   <div>
-  <h1 class="Itemtitle"> ${Menu.ItemName}</h1>
-  <
-  </div>
+  <h1 class="Itemtitle"> ${} </h1>
+  <h2 class="ItemPrice"> ${Menu.ItemName} </h2>
+  </div>npm
 `
 )
