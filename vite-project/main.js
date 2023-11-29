f1652ee114620d8ad915d9eeec16bd1cf4c9f240
AkashPak <144455859+AkashPak@users.noreply.github.com>
2023-11-29 08:48:58 -0500

    awdsadw

---
+++ vite-project/main.js
@@ -0,0 +1,23 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { DomSelectors } from './Dom.js'
+import { Menu } from './menu.js'
+
+
+
+
+
+
+
+function Insert(dfw){    
+    console.log(dfw)
+dfw.forEach((d)=> document.querySelector(".flex-container").insertAdjacentHTML("beforebegin",
+`<div>
+<h1 class="Itemtitle">${d.ItemName}</h1>
+<h2 class="ItemPrice">${d.ItemPrice}</h2>
+<img src= "${d.ItemImage}" alt="" class="ItemImage">
+</div>`));}
+
+
+document.querySelector(".whole").addEventListener("click", Insert(Menu))
