d79aba2ab555cdd93c144e02c4041176c1dc4358
AkashPak <akashp8@nycstudents.net>
2023-12-01 09:26:26 -0500

    daws

--- vite-project/main.js
+++ vite-project/main.js
@@ -18,5 +18,11 @@ dfw.forEach((d)=> document.querySelector(".flex-container").insertAdjacentHTML("
 <h2 class="ItemPrice">${d.ItemPrice}</h2>
 </div>`));}
 
-
 document.querySelector(".whole").addEventListener("click", Insert(Menu))
+
+const Appetis= Menu.filter((Monke)=>Monke.ItemCat.includes("Appetizer"));
+document.querySelector(".Appe").addEventListener("click",Insert(Appetis))
+
+const Fams= Menu.filter((gas)=>gas.ItemSize.includes("Family"));
+document.querySelector(".Family Serving").addEventListener("click",Insert(Fams))
+
